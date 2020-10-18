3fdbfe5bb68d9053cef46737547e671d4b55a144
Viacheslav-ed <viacheslav.vozniuk@gmail.com>
2020-10-18 16:51:25 +0300

    feat: connection morgan and winston to app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+const morgan = require('morgan');
+const logger = require('./common/winston');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
@@ -12,6 +14,8 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use(morgan('combined', { stream: logger.stream }));
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
---
+++ src/common/winston.js
@@ -0,0 +1,36 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'silly',
+  format: format.combine(format.colorize(), format.cli()),
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: `${__dirname}/../logs/error.log`,
+      level: 'error',
+      format: format.combine(format.uncolorize(), format.json())
+    }),
+    new transports.File({
+      filename: `${__dirname}/../logs/info.log`,
+      level: 'info',
+      format: format.combine(format.uncolorize(), format.json())
+    })
+  ]
+});
+
+// logger.silly('silly');
+// logger.debug('debug');
+// logger.verbose('verbose');
+// logger.info('info');
+// logger.warn('warn');
+// logger.error('error');
+//
+// logger.log('info', 'info from log');
+
+logger.stream = {
+  write(message) {
+    logger.info(message);
+  }
+};
+
+module.exports = logger;
