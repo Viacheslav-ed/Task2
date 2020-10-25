7a86a35eaca6f436fc9d11cb5f29d44e5c6f1845
Viacheslav-ed <viacheslav.vozniuk@gmail.com>
2020-10-25 20:35:01 +0200

    feat: add 404 to users collection

--- src/resources/users/user.mongo.repository.js
+++ src/resources/users/user.mongo.repository.js
@@ -1,12 +1,24 @@
 const User = require('./user.model');
 
 const getAll = async () => User.find({});
-const get = async id => User.findById(id);
+const get = async id => {
+  const user = await User.findById(id);
+  if (!user) {
+    const err = new Error(`User '${id}' not found`);
+    err.status = '404';
+    throw err;
+  }
+  return user;
+};
 const create = async user => User.create(user);
 const update = async (id, user) => {
+  await get(id);
   await User.updateOne({ _id: id }, user);
   return get(id);
 };
-const remove = async id => User.deleteOne({ _id: id });
+const remove = async id => {
+  await get(id);
+  await User.deleteOne({ _id: id });
+};
 
 module.exports = { getAll, get, create, update, remove };
